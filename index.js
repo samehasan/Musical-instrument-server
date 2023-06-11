d8d26c349943d931184d8627a446836f3d9dab41
samehasan <samehasan25@gmail.com>
2023-06-12 00:16:47 +0600

    1st

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -8,6 +9,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+  
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
+      }
+      req.decoded = decoded;
+      next();
+    })
+  }
 
 const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ssocgpa.mongodb.net/?retryWrites=true&w=majority`;
@@ -30,6 +47,14 @@ async function run() {
     const ClassesCollection = client.db("MILSDB").collection("Classes");
     const cartCollection = client.db("MILSDB").collection("carts");
 
+    app.post('/jwt', (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+  
+        res.send({ token })
+      })
+  
+
     app.get('/users', async (req, res) => {
         const result = await usersCollection.find().toArray();
         res.send(result);
@@ -47,6 +72,22 @@ async function run() {
         const result = await usersCollection.insertOne(user);
         res.send(result);
       });
+
+      app.patch('/users/admin/:id', async (req, res) => {
+        const id = req.params.id;
+        console.log(id);
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            role: 'admin'
+          },
+        };
+  
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+  
+      })
+  
 
     app.get('/instructor', async (req, res) => {
         const result = await InstructorCollection.find().toArray();
