c6d32e7e3f539c2b50e788e81d28d8c9529d0cde
samehasan <samehasan25@gmail.com>
2023-06-11 20:35:14 +0600

    1st

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ssocgpa.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,10 +25,28 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
+    const usersCollection = client.db("MILSDB").collection("users");
     const InstructorCollection = client.db("MILSDB").collection("Instructor");
     const ClassesCollection = client.db("MILSDB").collection("Classes");
     const cartCollection = client.db("MILSDB").collection("carts");
+
+    app.get('/users', async (req, res) => {
+        const result = await usersCollection.find().toArray();
+        res.send(result);
+      });
+
+    app.post('/users', async (req, res) => {
+        const user = req.body;
+        const query = { email: user.email }
+        const existingUser = await usersCollection.findOne(query);
+  
+        if (existingUser) {
+          return res.send({ message: 'user already exists' })
+        }
+  
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      });
 
     app.get('/instructor', async (req, res) => {
         const result = await InstructorCollection.find().toArray();
@@ -40,11 +58,31 @@ async function run() {
         res.send(result);
       })
 
+      app.get('/carts', async (req, res) => {
+        const email = req.query.email;
+  
+        if (!email) {
+          res.send([]);
+        }
+        const query = { email: email };
+        const result = await cartCollection.find(query).toArray();
+        res.send(result);
+      });
+  
       app.post('/carts', async (req, res) => {
         const item = req.body;
+        console.log(item);
         const result = await cartCollection.insertOne(item);
         res.send(result);
       })
+  
+      app.delete('/carts/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await cartCollection.deleteOne(query);
+        res.send(result);
+      })
+  
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
